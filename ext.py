a1d657e98b9369f6efe9f4e967b2c4579fac91de
hua.wang <hua.wang@17zuoye.com>
2019-10-11 18:05:37 +0800

    1

--- ext.py
+++ ext.py
@@ -1,9 +1,10 @@
 from os import system
 
+
 class Ext1:
-    def __init__(self, supervisord):
+    def __init__(self, supervisord, config):
         self.supervisord = supervisord
-        self.args = args
+        self.args = config
 
     def add(self, num1: int, num2: int):
         return num1 + num2
@@ -11,6 +12,11 @@ class Ext1:
     def ls(self):
         return system('ls')
 
+    def hello(self):
+        return 'hello'
 
-def myext(supervisord):
-    return Ext1(supervisord)
+
+def myext(supervisord, **config):
+    retries = int(config.get('retries', 0))
+    ext1 = Ext1(supervisord, retries)
+    return ext1
